5196678cf5c6ea1bba37040f0d9b240e630f1b5c
Halvor Risto <64halvor64@gmail.com>
2019-11-19 01:29:39 +0100

    Added ternary to binary

--- conversion matrix generator/main.cpp
+++ conversion matrix generator/main.cpp
@@ -1,17 +1,26 @@
+// Conversion matrix generator for binary and ternary
+// Author: Halvor Nybø Risto
+// This program uses two different methods for generating the conversion matrices.
+// For binary to ternary, a method nicknamed the "input bit-flipping method" is implemented.
+// This method was use to demonstrate that it can be done in this way.
+// For the ternary to binary method, an arguably much simpler method is used, nicknamed the Digit Relation method
+// NOTE: This program only works up to 64 bits
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <math.h>
 #include <cstdint>
 using namespace std;
-
-//vectors are used since they have dynamic size
 vector<int> input; //binary inputs 
 vector<int> all_one_ternary;
 vector<int> output; // ternary outputs
-//vector<int> matrix; //2D vector
-int matrix[128][82];
-
+					//vector<int> matrix; //2D vector
+// matrices are larger than 64 bit / 41 trit and therefore do not need a dynamic size
+int binToTerMatrix[128][82]; // binary to ternary 
+int terToBinMatrix[82][128]; // ternary to binary
+int bits = 0;
+int trits = 0;
 int getValue(string prompt, int low, int high) {	// gets a value (int) between low and high and returns it
 	int value = 0;
 	do {
@@ -30,7 +39,7 @@ int getValue(string prompt, int low, int high) {	// gets a value (int) between l
 
 void setOutput(int bits) {
 	int trits = ceil(bits / 1.585);
-	uint64_t decimal = 0;				
+	uint64_t decimal = 0;
 	for (int i = 0; i < bits; i++) {
 		decimal += ((1 << i) * (!input[i]));
 	}
@@ -51,7 +60,7 @@ void setAllOneTernary(int bits) {	//sets the all_one_ternary to the balanced ter
 	int trits = ceil(bits / 1.585);
 	uint64_t decimal = 0;
 	for (int i = 0; i < bits; i++) decimal += 1 << i;//pow(2, i);
-	//cout << "decimal: " << decimal << "\n\n";
+													 //cout << "decimal: " << decimal << "\n\n";
 
 	uint64_t valueZero = 0;
 	for (int i = 0; i < trits; i++) valueZero += pow(3, i);
@@ -64,115 +73,157 @@ void setAllOneTernary(int bits) {	//sets the all_one_ternary to the balanced ter
 		decimal = decimal / 3;
 		if (decimal == 0) i = trits;
 	}
-
-
 }
 
 
 int main() {
 	while (true) {
+		if (getValue("Select a converter\n1. Binary to ternary\n2. Ternary to binary\n>", 1, 2) == 1) { // binary to ternary
+			bits = getValue("enter number of binary inputs (0 - 64): ", 0, 64);
+			trits = ceil(bits / 1.585); //a trit can replace 1.58496 bits
+											
+			cout << "\n";
 
-		int bits = getValue("enter number of binary inputs (0 - 64): ", 0, 64);
-		int trits = ceil(bits / 1.585); //a trit can replace 1.58496 bits
-		//NOTE: In implementation, it is advisable to use one output trit more than the number of trits in the matrix.
-		// The most significant bit can have a positive overflow
-		// this is due to the fact that 2 trit balaced ternary can count from -4 to +4, 
-		// while 2 trit unsigned can count from 0 to 8.
-		// so while the number of trits defined above are enough for the matrix, it might sometimes not be enough for some specific outputs.
-		// for some data sizes, the overhead might cause this positive overflow to never happen. 
-
-		// reserving vector space and setting contents to 0
-		output.reserve(trits);
-		input.reserve(bits);
-		all_one_ternary.reserve(trits);
-		//matrix.reserve(trits); ///////////////////////////
-		for (int i = 0; i < bits; i++) {
-			input.push_back(0);
-		}
-		for (int i = 0; i < trits; i++) {
-			output.push_back(0);
-			all_one_ternary.push_back(0);
-			//	matrix.push_back(0);////////////////////////////
-		}
+			// reserving vector space and setting contents to 0
+			output.reserve(trits);
+			input.reserve(bits);
+			all_one_ternary.reserve(trits);
+			//matrix.reserve(trits); ///////////////////////////
+			for (int i = 0; i < bits; i++) {
+				input.push_back(0);
+			}
+			for (int i = 0; i < trits; i++) {
+				output.push_back(0);
+				all_one_ternary.push_back(0);
+				//	matrix.push_back(0);////////////////////////////
+			}
 
 
 
-		setAllOneTernary(bits);
+			setAllOneTernary(bits);
 
 
 
-		for (int n = 0; n < bits; n++) input[n] = 0;
-		input[0] = 1;
+			for (int n = 0; n < bits; n++) input[n] = 0;
+			input[0] = 1;
 
-		for (int i = 0; i < bits; i++) {
-			//cout << "\n\ni: " << i << "\n";
-			setOutput(bits);
+			for (int i = 0; i < bits; i++) {
+				//cout << "\n\ni: " << i << "\n";
+				setOutput(bits);
+
+				for (int j = 0; j < trits; j++) {
+					//cout << "\n\nj: " << j << "\n";
+					//cout << "input: ";
+					//for (int foobar = bits - 1; foobar > -1; foobar--) cout << !input[foobar];
+					//cout << "\n\n\n";
+					binToTerMatrix[i][j] = all_one_ternary[j] - output[j];
+					//cout << "all_one_ternary["<<j<<"] = " << all_one_ternary[j] << "\n";
+					//cout << "output[" << j << "] = " << output[j] << "\n";
+
+				}
+
+				for (int k = bits - 1; k > 0; k--) input[k] = input[k - 1];
+				input[0] = 0;
+
+				//cout << "\n";
+			}
 
 			for (int j = 0; j < trits; j++) {
-				//cout << "\n\nj: " << j << "\n";
-				//cout << "input: ";
-				//for (int foobar = bits - 1; foobar > -1; foobar--) cout << !input[foobar];
-				//cout << "\n\n\n";
-				matrix[i][j] = all_one_ternary[j] - output[j];
-				//cout << "all_one_ternary["<<j<<"] = " << all_one_ternary[j] << "\n";
-				//cout << "output[" << j << "] = " << output[j] << "\n";
+				for (int i = bits - 1; i > -1; i--) {
+					if (binToTerMatrix[i][j] == -2) {
+						binToTerMatrix[i][j] = 1;
+						binToTerMatrix[i][j + 1] += -1;
+					}
+					else if (binToTerMatrix[i][j] == 2) {
+						binToTerMatrix[i][j] = -1;
+						binToTerMatrix[i][j + 1] += +1;
+					}
+					else if (binToTerMatrix[i][j] == 3) {
+						binToTerMatrix[i][j] = 0;
+						binToTerMatrix[i][j + 1] += +1;
+					}
+					else if (binToTerMatrix[i][j] == -3) {
+						binToTerMatrix[i][j] = 0;
+						binToTerMatrix[i][j + 1] += -1;
+					}
 
+				}
 			}
 
-			for (int k = bits - 1; k > 0; k--) input[k] = input[k - 1];
-			input[0] = 0;
-
-			//cout << "\n";
-		}
-
-		for (int j = 0; j < trits; j++) {
-			for (int i = bits - 1; i > -1; i--) {
-				if (matrix[i][j] == -2) {
-					matrix[i][j] = 1;
-					matrix[i][j + 1] += -1;
-				}
-				else if (matrix[i][j] == 2) {
-					matrix[i][j] = -1;
-					matrix[i][j + 1] += +1;
-				}
-				else if (matrix[i][j] == 3) {
-					matrix[i][j] = 0;
-					matrix[i][j + 1] += +1;
-				}
-				else if (matrix[i][j] == -3) {
-					matrix[i][j] = 0;
-					matrix[i][j + 1] += -1;
-				}
-
+			cout << "    ";
+			for (int i = 0; i < bits; i++) {
+				cout << "b" << i << " ";
+				if (i < 10) cout << " ";
 			}
 			cout << "\n";
-		}
-
-
-		for (int j = 0; j < trits; j++) {
-			cout << "\nt" << j << ": ";
-			for (int i = bits - 1; i > -1; i--) {
-				cout << matrix[i][j] << "  ";
+			for (int j = 0; j < trits; j++) {
+				cout << "\nt" << j << "= ";
+				if (j < 10) cout << " ";
+				for (int i = bits - 1; i > -1; i--) {
+					if (binToTerMatrix[i][j] == -1) cout << "-   ";
+					if (binToTerMatrix[i][j] == +1) cout << "+   ";
+					if (binToTerMatrix[i][j] == 0) cout << "0   ";
+				}
+				cout << "\n";
 			}
-			cout << "\n";
-		}
 
 
 
-		/*
-		cout << "all_one_ternary: " << "\n";
-		for (int i = 0 ; i < trits; ++i) {
+			/*
+			cout << "all_one_ternary: " << "\n";
+			for (int i = 0 ; i < trits; ++i) {
 			int & foobar = all_one_ternary[i];
 			cout << foobar << "\n";
+			}
+			cout << "\n\n";
+			*/
+
+			cout << "\n\n\n";
+
+			output.clear();
+			input.clear();
+			all_one_ternary.clear();
 		}
-		cout << "\n\n";
-		*/
+		else {	// ternary to binary
+			
+			bits = getValue("enter number of binary outputs (0 - 64): ", 0, 64);
+			trits = ceil(bits / 1.585); //a trit can replace 1.58496 bits
+			vector<int> ternaryExponents;
+			ternaryExponents.reserve(trits);
+			for (int i = 0; i < trits + 1; i++) {
+				ternaryExponents.push_back(pow(3,i));
+			}
+			cout << "\n\n";
+			
 
-		cout << "\n\n\n";
+			
+			for (int m = 0; m < trits; m++) {
+				for (int n = 0; n < bits; n++) {
+					binToTerMatrix[trits - m - 1][bits - n - 1] = ternaryExponents[m] % 2;
+					ternaryExponents[m] = ternaryExponents[m] / 2;
+				}
+			}
 
-		output.clear();
-		input.clear();
-		all_one_ternary.clear();
+
+
+			cout << "    ";
+			for (int m = 0; m < trits; m++) {
+				cout << "t" << m << " ";
+				if (m < 10) cout << " ";
+			}
+			cout << "\n";
+			for (int n = bits -1; n > -1; n--) {
+				cout << "b" << 15 - n << "= ";
+				if ((15 - n) < 10) cout << " ";
+				for (int m = 0; m < trits; m++) {
+					cout << binToTerMatrix[m][n]; 
+					if (m < trits - 1) cout << " + ";
+				}
+				cout << "\n";
+			}
+			cout << "\n\n\n";
+		}
+		
 
 
 	}
