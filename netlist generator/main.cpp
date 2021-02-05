dea009f108eb2145866ef44cfd3b2c2dab855eae
Halvor Risto <42732697+Halvor64@users.noreply.github.com>
2021-02-05 16:02:06 +0100

    Update main.cpp
    changed 6 to 9
    (for 3 input bug)

--- netlist generator/main.cpp
+++ netlist generator/main.cpp
@@ -85,7 +85,7 @@ void enterData() {
         }
 
         // entering subcircuit interface data
-        for (int j = 0; j < 6; j++) {
+        for (int j = 0; j < 9; j++) {
             netlists[i].setInverters(j, inv[i][j]);
         }
     }
